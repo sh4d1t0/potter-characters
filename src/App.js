241424ef215844ec4f6b43d78e7dbc581f6608aa
sh4d1t0 <shadito@live.com.mx>
2021-12-16 16:55:12 -0600

    Agregando botones y modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Routes, Route, Link as RouterLink } from 'react-router-dom'
 import { Counter } from './features/counter/Counter'
 import {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,49 @@
-import { Box, Grid, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Grid,
+  Modal,
+  Stack,
+  TextField,
+  Typography
+} from '@mui/material'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import getCharacters from '../../api/hp-charactersApi'
 import { setCharactersList } from '../../features/characters/charactersSlice'
 import CharacterList from '../character/CharacterList'
+import BookmarkIcon from '@mui/icons-material/Bookmark'
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt'
+import { useFormik } from 'formik'
 
 const Home = () => {
   const dispatch = useDispatch()
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: 'foobar@example.com',
+      password: 'foobar'
+    },
+    //validationSchema: validationSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    }
+  })
 
   useEffect(() => {
     const fetchAllCharacters = async () => {
@@ -19,22 +56,87 @@ const Home = () => {
   }, [])
 
   return (
-    <>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Grid item xs={12}>
-          <Typography component="div" variant="h3">
-            <Box sx={{ textAlign: 'center', m: 1 }}>Personajes</Box>
-          </Typography>
-        </Grid>
-        <CharacterList />
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+    >
+      <Grid item xs={12}>
+        <Button variant="contained" endIcon={<BookmarkIcon />}>
+          FAVORITOS
+        </Button>
+        <Button
+          variant="contained"
+          endIcon={<PersonAddAltIcon />}
+          onClick={handleOpen}
+        >
+          AGREGAR
+        </Button>
       </Grid>
-    </>
+      <Grid item xs={12}>
+        <Typography component="div" variant="h4">
+          <Box sx={{ textAlign: 'center', m: 1 }}>Selecciona tu Filtro</Box>
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Stack spacing={2} direction="row">
+          <Button
+            variant="outlined"
+            onClick={() => {
+              console.log('estudiantes')
+            }}
+          >
+            ESTUDIANTES
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              console.log('staff')
+            }}
+          >
+            STAFF
+          </Button>
+        </Stack>
+      </Grid>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              id="email"
+              name="email"
+              label="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Modal>
+      <CharacterList />
+    </Grid>
   )
 }
 
