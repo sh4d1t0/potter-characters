482ae5bdad4120a000d4d2c6407512a4dca95084
sh4d1t0 <shadito@live.com.mx>
2021-12-17 04:45:04 -0600

    Link router ya no se usa en App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link as RouterLink } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { CssBaseline, Container } from '@mui/material'
 import Home from './components/Home/Home'
 import Staff from './components/character/CharacterStaffList'
